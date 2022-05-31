30497436231a0337038c9c04b973fd19f7b76223
Wren <renprofessional97@gmail.com>
2022-05-31 12:06:00 +0100

    Working on project

--- davePet.js
+++ davePet.js
@@ -1,28 +1,97 @@
-nmp install inquirer
+const inquirer = require("inquirer");
 
 class Animal {
-  constructor(species) {
-  this.species = species;
-  this.health = 100;
-  this.hunger = 100;
+  constructor(name) {
+    this.name = name;
+    this.health = 100;
+    this.hunger = 100;
   }
-  walks() {
-  this.health += 10;
-  this.hunger -= 10;
-  console.log(`${this.species}'s health is ${this.health} and hunger is ${this.hunger}`);
-  return this;
+  drinks() {
+    this.health += 5;
+    return this;
   }
-  food() {
-  this.health += 10;
-  this.hunger += 10;
-  console.log(`${this.species}'s health is ${this.health} and hunger is ${this.hunger}`);
-  return this;
+
+  eats() {
+    this.health += 5;
+    this.hunger += 10;
+    console.log(`${this.name}'s health is ${this.health}`);
+    return this;
   }
   stats() {
-  return console.table({
-  species: this.species,
-  health: this.health,
-  hunger: this.hunger,
+    return console.table({
+      name: this.name,
+      health: this.health,
+      hunger: this.hunger,
+    });
+  }
+}
+
+class Cat extends Animal {
+  constructor(name, content) {
+    super(name, content);
+    this.content = content;
+  }
+
+  playWool() {
+    this.health += 10;
+    this.hunger -= 10;
+    console.log(`${this.name} is happy`);
+    return this;
+  }
+  naps() {
+    console.log(
+      `${this.name} is taking a lovely nap, they are ${this.content}`
+    );
+    this.health += 10;
+    return this;
+  }
+}
+
+class Dog extends Animal {
+  constructor(name, content) {
+    super(name, content);
+    this.content = content;
+  }
+
+  playBall() {
+    this.health += 10;
+    this.hunger -= 10;
+    console.log(`${this.name} is happy`);
+    return this;
+  }
+  walks() {
+    console.log(`Taking ${this.name} for a walk, they are ${this.content}`);
+    this.health += 10;
+    return this;
+  }
+}
+
+let myPet = "";
+const start = async () => {
+  const typeOfPet = await inquirer.prompt({
+    name: "typeOfPet",
+    type: "list",
+    message: "What is your pets name?",
+    choices: [
+      {
+        key: "a",
+        name: "Cat",
+        value: "cat",
+      },
+      {
+        key: "b",
+        name: "Dog",
+        value: "dog",
+      },
+    ],
   });
-}
-}
+  const petName = await inquirer.prompt({
+    type: `input`,
+    name: `petName`,
+    message: `What will you name your pet?`,
+  });
+  if (typeOfPet === `cat`) myPet = new Cat(petName);
+  else if (typeOfPet === `dog`) myPet = new Dog(petName);
+};
+
+console.log(start())
