ec1480951628dcc94bd58aaf3827aade54cf18ef
Wren <renprofessional97@gmail.com>
2022-06-01 18:47:49 +0100

    Finished version

--- app.js
+++ app.js
@@ -1,56 +1,59 @@
 const inquirer = require("inquirer");
-const { questions } = require("./questions")
-const {Animal, Dog, Cat} = require("./cyberPet")
+const { Dog, Cat } = require("./cyberPet");
 
-
-const start = async () => {
-    const typeOfPet = await inquirer.prompt({
-        type: "list",
-        name: "pet",
-        message: "Which Pet would you like to adopt?",
-        choices: ["cat", "dog",]
-    });
-
-    const petName = await inquirer.prompt({
-        type: "input",
-        name: "name",
-        message: `What will you call your new ${typeOfPet.pet}?`
-    });
-
-    if (typeOfPet === "cat") {
-        myPet = new Cat(petName.name);
-        options = [{
-            key: "A",
-            name: "Give Food",
-            value: "food",
-        },
-        {
-            key: "B",
-            name: "Give Drink",
-            value: "drink",
-        },
-        {
-            key: "C",
-            name: "Play",
-            value: "play",
-        },
-        {
-            key: "D",
-            name: "Sleep",
-            value: "sleep",
-        }
-        ];
-    } else if (typeOfPet === "dog") {
-        myPet = new Dog(petName.name);
-        options = [{
-            key: "A",
-            name: "Give Food",
-            value: "food",
-        },
-        {
-            key: "B",
-            name: "Give Drink",
-            value: "drink",
+try {
+    let myPet = "";
+    let options = [];
+    
+    const start = async (callback) => {
+        
+        const typeOfPet = await inquirer.prompt({
+            type: "list",
+            name: "typeOfPet",
+            message: "Which pet would you like to adopt?",
+            choices: ["cat", "dog"],
+        });
+            
+        const nameOfPet = await inquirer.prompt({
+            type: "input",
+            name: "nameOfPet",
+            message: `What will you call your new ${typeOfPet.typeOfPet}?`,
+        });
+        
+        if (typeOfPet.typeOfPet === "cat") {
+            myPet = new Cat(nameOfPet.nameOfPet);
+            options = [{
+                key: "A",
+                name: "Give Food",
+                value: "food",
+            },
+            {
+                key: "B",
+                name: "Give Drink",
+                value: "drink",
+            },
+            {
+                key: "C",
+                name: "Play",
+                value: "play",
+            },
+            {
+                key: "D",
+                name: "Sleep",
+                value: "sleep",
+            },
+            ];
+        } else if (typeOfPet === "dog") {
+            myPet = new Dog(nameOfPet.nameOfPet);
+            options = [{
+                key: "A",
+                name: "Give Food",
+                value: "food",
+            },
+            {
+                key: "B",
+                name: "Give Drink",
+                value: "drink",
             },
             {
                 key: "C",
@@ -61,26 +64,43 @@ const start = async () => {
                 key: "D",
                 name: "Wag Tail",
                 value: "wagTail",
-            },
-            ]
-    }
-    const activity = await inquirer.prompt({
-        type: "list",
-        name: "chosen",
-        message: `What do you want to do with ${myPet.name}?`,
-        choices: options,
-    })
+            }
+            ];
+        }
 
-    switch (activity.chosen) {
-        case "food":
-            myPet.food();
-            break;
-        case "drink":
-            myPet.drink();
-            break;
-        case "walk":
-            myPet.walk();
-            break;
+        callback(game)     
+            
     }
-};
-start()
+
+
+    const game = async(callback) => {
+        const activity = await inquirer.prompt({
+            type: "list",
+            name: "chosen",
+            message: `What do you want to do with ${myPet.name}?`,
+            choices: options,
+        });
+            
+        switch (activity.chosen) {
+            case "food":
+                myPet.food();
+                break;
+            case "drink":
+                myPet.drink();
+                break;
+            case "fetch":
+                myPet.fetch();
+                break;
+            case "wagTail":
+                myPet.wagTail();
+                break;
+        }
+                    
+        callback(game)
+    }
+
+    start(game);
+
+} catch (error){
+    console.log("Error Fecth", error)
+}
